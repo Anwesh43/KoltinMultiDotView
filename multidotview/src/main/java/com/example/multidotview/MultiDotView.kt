c6d4253f7509b09770ab01b9e88d38b2c14a6418
Anwesh43 <anweshthecool0@gmail.com>
2018-05-19 10:59:59 +0530

    adding MultiDotView class

--- multidotview/src/main/java/com/example/multidotview/MultiDotView.kt
+++ multidotview/src/main/java/com/example/multidotview/MultiDotView.kt
@@ -3,3 +3,26 @@ package com.example.multidotview
 /**
  * Created by anweshmishra on 19/05/18.
  */
+
+import android.content.*
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
+
+class MultiDotView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
